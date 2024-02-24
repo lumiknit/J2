35699a5f6316f2527f0dc301a7446a85cee91d7f
lumiknit <aasr4r4@gmail.com>
2024-02-24 20:55:53 +0900

    Change prefix rule

--- src/path.rs
+++ src/path.rs
@@ -21,18 +21,13 @@ pub fn convert_base_paths_to_names(base_paths: &Vec<String>) -> Vec<PathItem> {
       .filter(|b| !b.is_empty())
       .last()
       .unwrap();
-    let mut ln_iter = last_name.chars();
-    let mut name = "".to_string();
+    let mut name = last_name.to_string();
+    let mut c = 0;
 
-    loop {
-      let c = if let Some(c) = ln_iter.next() { c } else { '_' };
-      name.push(c);
-
-      if !set.contains(&name) {
-        break;
-      }
+    while set.contains(&name) {
+      name = format!("{}-{}", last_name, c);
+      c += 1;
     }
-
     set.insert(name.clone());
 
     paths.push(PathItem {
